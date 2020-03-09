09b15f22615f2d7a62efdceecc89e7c6ba8555c8
cool8676 <cool8676@gmail.com>
2020-03-10 02:01:40 +0800

    insatll AASM

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise', '~> 4.7', '>= 4.7.1'
 gem 'font_awesome5_rails', '~> 1.0'
+gem 'aasm', '~> 4.11'
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,11 +1,29 @@
 class Story < ApplicationRecord
+  include AASM
   belongs_to :user
   validates :title, presence: true
+
 
   #Display not-deleted stories
   default_scope { where(deleted_at: nil )}
 
   def destroy
     self.update(deleted_at: Time.now)
+  end
+
+  aasm(column: :status, no_direct_assignment: true) do
+    state :draft, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :draft, to: :published
+      after do
+        puts "簡訊通知"
+      end
+    end
+
+    event :unpublish do
+      transitions from: :published, to: :draft
+    end
   end
 end
