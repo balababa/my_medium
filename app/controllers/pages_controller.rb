14c99485fb74a9bef6b0a79b3d43918cef7fdf5c
cool8676 <cool8676@gmail.com>
2020-03-13 21:58:46 +0800

    Avoid activerecord N+1 query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,9 @@ class PagesController < ApplicationController
     # @stories = Story.order(created_at: :desc).includes(:user)
     # @stories = Story.where(status: :published).order(created_at: :desc).includes(:user)
     # @stories = Story.published_stories.order(created_at: :desc).includes(:user)
-    @stories = Story.published.order(created_at: :desc).includes(:user)
+    # @stories
+    # @stories = published.with_attached_cover_image.order(created_at: :desc).includes(:user) 
+    @stories = Story.published_stories
   end
   def show
   end
